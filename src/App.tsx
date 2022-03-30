a2fbebdd07435bf56a50d8b4f9d0a708a8259580
Filipe Dias <filipediascontato@gmail.com>
2022-03-29 23:32:27 -0300

    Creates the header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+import Board from "./components/Board";
 import Header from "./components/Header";
 import GlobalStyle from "./styles/global";
 
 function App() {
   return (
     <>
+      <Header />
+      <Board />
+
       <GlobalStyle />
     </>
   );
