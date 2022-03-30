3dcd48a1d1911aa605fd8d46c34c06b7537b9442
Filipe Dias <filipediascontato@gmail.com>
2022-03-29 23:30:42 -0300

    Creates the List component

---
+++ src/components/List/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { MdAdd } from "react-icons/md";
+
+import Card from "../Card";
+import { Container } from "./styles";
+
+interface Props {
+  data: List;
+}
+
+interface List {
+  title: string;
+  creatable: boolean;
+  cards: Card[];
+  done?: boolean;
+}
+
+interface Card {
+  id: number;
+  content: string;
+  labels: string[];
+  user?: string;
+}
+
+const List: React.FC<Props> = ({ data }: Props) => {
+  return (
+    <Container done={data.done}>
+      <header>
+        <h2>{data.title}</h2>
+        {data.creatable && (
+          <button type="button">
+            <MdAdd size={24} color="#fff" />
+          </button>
+        )}
+      </header>
+
+      <ul>
+        {data.cards.map((card) => (
+          <Card key={card.id} data={card} />
+        ))}
+      </ul>
+    </Container>
+  );
+};
+
+export default List;
